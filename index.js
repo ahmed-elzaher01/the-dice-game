a8434f9c0a0c4dc460049cad282c4f6580696f1e
ahmed-elzaher01 <es.ahmed.alzaher2023@alexu.edu.eg>
2025-11-28 00:36:55 +0200

    play btn added

--- index.js
+++ index.js
@@ -1,81 +1,85 @@
-let x = 0;
-let y = 0;
-x = Math.floor(Math.random() * 6) + 1;
+function dice() {
+  let x = Math.floor(Math.random() * 6) + 1;
 
-y = Math.floor(Math.random() * 6) + 1;
+  let y = Math.floor(Math.random() * 6) + 1;
+  switch (x) {
+    case 1:
+      document
+        .querySelector(".firstDice")
+        .setAttribute("src", "./images/dice1.png");
+      break;
+    case 2:
+      document
+        .querySelector(".firstDice")
+        .setAttribute("src", "./images/dice2.png");
+      break;
+    case 3:
+      document
+        .querySelector(".firstDice")
+        .setAttribute("src", "./images/dice3.png");
+      break;
+    case 4:
+      document
+        .querySelector(".firstDice")
+        .setAttribute("src", "./images/dice4.png");
+      break;
+    case 5:
+      document
+        .querySelector(".firstDice")
+        .setAttribute("src", "./images/dice5.png");
+      break;
+    case 6:
+      document
+        .querySelector(".firstDice")
+        .setAttribute("src", "./images/dice6.png");
+      break;
+  }
 
-switch (x) {
-  case 1:
-    document
-      .querySelector(".firstDice")
-      .setAttribute("src", "./images/dice1.png");
-    break;
-  case 2:
-    document
-      .querySelector(".firstDice")
-      .setAttribute("src", "./images/dice2.png");
-    break;
-  case 3:
-    document
-      .querySelector(".firstDice")
-      .setAttribute("src", "./images/dice3.png");
-    break;
-  case 4:
-    document
-      .querySelector(".firstDice")
-      .setAttribute("src", "./images/dice4.png");
-    break;
-  case 5:
-    document
-      .querySelector(".firstDice")
-      .setAttribute("src", "./images/dice5.png");
-    break;
-  case 6:
-    document
-      .querySelector(".firstDice")
-      .setAttribute("src", "./images/dice6.png");
-    break;
+  switch (y) {
+    case 1:
+      document
+        .querySelector(".secondDice")
+        .setAttribute("src", "./images/dice1.png");
+      break;
+    case 2:
+      document
+        .querySelector(".secondDice")
+        .setAttribute("src", "./images/dice2.png");
+      break;
+    case 3:
+      document
+        .querySelector(".secondDice")
+        .setAttribute("src", "./images/dice3.png");
+      break;
+    case 4:
+      document
+        .querySelector(".secondDice")
+        .setAttribute("src", "./images/dice4.png");
+      break;
+    case 5:
+      document
+        .querySelector(".secondDice")
+        .setAttribute("src", "./images/dice5.png");
+      break;
+    case 6:
+      document
+        .querySelector(".secondDice")
+        .setAttribute("src", "./images/dice6.png");
+      break;
+  }
+  if (x > y) {
+    document.querySelector("h1").innerHTML = " 🏆 Player 1 wins ";
+    document.querySelector(".p1").innerHTML = " Player 1 😎";
+    document.querySelector(".p2").innerHTML = " Player 2 ";
+  } else if (y > x) {
+    document.querySelector("h1").innerHTML = "Player 2 wins 🏆 ";
+    document.querySelector(".p2").innerHTML = " Player 2 😁";
+    document.querySelector(".p1").innerHTML = " Player 1 ";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw ";
+    document.querySelector(".p1").innerHTML = " Player 1 😐";
+    document.querySelector(".p2").innerHTML = " Player 2 😐";
+  }
 }
 
-switch (y) {
-  case 1:
-    document
-      .querySelector(".secondDice")
-      .setAttribute("src", "./images/dice1.png");
-    break;
-  case 2:
-    document
-      .querySelector(".secondDice")
-      .setAttribute("src", "./images/dice2.png");
-    break;
-  case 3:
-    document
-      .querySelector(".secondDice")
-      .setAttribute("src", "./images/dice3.png");
-    break;
-  case 4:
-    document
-      .querySelector(".secondDice")
-      .setAttribute("src", "./images/dice4.png");
-    break;
-  case 5:
-    document
-      .querySelector(".secondDice")
-      .setAttribute("src", "./images/dice5.png");
-    break;
-  case 6:
-    document
-      .querySelector(".secondDice")
-      .setAttribute("src", "./images/dice6.png");
-    break;
-}
-
-if (x > y) {
-  document.querySelector("h1").innerHTML = " 🏆 Player 1 wins ";
-  document.querySelector(".p1").innerHTML = " Player 1 😎🔥";
-} else if (y > x) {
-  document.querySelector("h1").innerHTML = "Player 2 wins 🏆 ";
-  document.querySelector(".p2").innerHTML = " Player 1 😁🎉";
-} else {
-  document.querySelector("h1").innerHTML = "Draw ";
-}
+document.querySelector("#b").addEventListener("click", dice);
